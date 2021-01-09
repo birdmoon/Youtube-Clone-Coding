c2bf5e63e6d552eef70f45ab579a4cf0cdef2bce
birdmoon <xeragara@gmail.com>
2021-01-09 20:24:37 +0900

    learned for get, post method

--- index.js
+++ index.js
@@ -7,4 +7,16 @@ function handleListening() {
   console.log(`Listening on: http://localhost:${PORT}`);
 }
 
+function handleHome(req, res) {
+  res.send("Hello from home");
+}
+
+function handleProfile(req, res) {
+  res.send("You are on my profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening); //locallhost:4000번으로 설정한다. 숫자가 바뀌면 브라우저에 그 숫자를 넣으면 됨.
