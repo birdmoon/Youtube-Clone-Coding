14770ee60d46fdf5d0a99f87171508edf800caf3
birdmoon <birdmoon@nestory.com>
2021-01-17 19:42:46 +0900

    pug 전역변수 미들웨어 설정 강의 2.16

--- app.js
+++ app.js
@@ -3,21 +3,23 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-// import { userRouter } from "./routers/userRouter"; //router파일에서 디폴트로 export한게 아니라 임포트 시키는 문법이 다르다. (변수처리)
+import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 
+// import { userRouter } from "./routers/userRouter"; //router파일에서 디폴트로 export한게 아니라 임포트 시키는 문법이 다르다. (변수처리)
 // const express = require("express"); //node_modules폴더에 express폴더를 가져온다. 이를 최신버전의 JS로 표현하면 아래와 같다.
 const app = express();
 
+app.use(helmet()); //application이 더 안전하도록 만들어준다.
 app.set("view engine", "pug"); //express api가이드를 참고하여 pug란 벨류의 view engine 속성을 세팅한다.
 app.use(cookieParser()); //쿠키를 전달받아서 사용할 수 있도록 만들어주는 미들웨어. 사용자 인증 같은 곳에서 쿠키를 검사할 때 사용한다.
 app.use(bodyParser.json()); //bodyParser는 사용자가 웹사이트로 전달하는 정보들을 검사하는 미들웨어. request 정보에서 form이나 json형태로 된 body를 검사한다.
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet()); //application이 더 안전하도록 만들어준다.
 app.use(morgan("dev")); //application에서 발생하는 모든 일들을 logging하는 미들웨어
+app.use(localsMiddleware); //middleware.js에 만들어놓은 localsMiddleware함수는 globalRouter, userRouter, videoRouter보다 상위에 위치해 있어야 한다.
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter); //use는 누군가 use경로로 오면 이 라우터 전체를 사용하겠다는 의미임. 누군가 routes.users로 접속하면 userRouter를 사용하겠다.
---
+++ middlewares.js
@@ -0,0 +1,10 @@
+import routes from "./routes";
+
+export const localsMiddleware = (req, res, next) => {
+  res.locals.siteName = "WeTube"; //미들웨어에서 locals에 siteName을 추가했다.
+  res.locals.routes = routes; //routes에 routes.js 객체를 추가.
+  next();
+};
+
+//전역적으로 사용할 수 있는 미들웨어 함수를 만들어봤다. 변수를 사용하고 싶으면 여기다 적용하면 된다.
+//템플릿, 뷰, 모든 곳에서 사용가능함.
